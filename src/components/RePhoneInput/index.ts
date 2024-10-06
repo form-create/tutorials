c2803d86ebd4e3c5e8507256e752f9934a4c5d61
Arayui <18668792456@163.com>
2024-10-06 20:34:45 +0800

    feat: add re-phone-input component

---
+++ src/components/RePhoneInput/index.ts
@@ -0,0 +1,7 @@
+import rePhoneInput from "./src/index.vue";
+import { withInstall } from "@pureadmin/utils";
+
+/** 自定义 手机号 输入框 */
+export const RePhoneInput = withInstall(rePhoneInput);
+
+export default RePhoneInput;
