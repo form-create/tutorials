309d8dd7adde2f372406e81c2afa55d893a81cbb
Arayui <18668792456@163.com>
2024-11-03 16:45:02 +0800

    feat: (mock) add interface /options

---
+++ mock/fetch.ts
@@ -0,0 +1,29 @@
+import { defineFakeRoute } from "vite-plugin-fake-server/client";
+
+export default defineFakeRoute([
+  {
+    url: "/options",
+    method: "get",
+    response(processedRequest, _, response) {
+      const { headers } = processedRequest;
+      if (headers["authorization"] === "Bearer token") {
+        return {
+          code: 200,
+          message: "ok",
+          data: {
+            admin: "管理员",
+            common: "普通用户",
+            tester: "测试用户"
+          }
+        };
+      } else {
+        response.statusCode = 401;
+        return {
+          code: 401,
+          message: "forbid",
+          data: null
+        };
+      }
+    }
+  }
+]);
