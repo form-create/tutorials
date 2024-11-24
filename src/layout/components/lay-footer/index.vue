7edab8936543b87f140839e610119056da1277aa
Arayui <18668792456@163.com>
2024-11-24 15:45:36 +0800

    docs: revise pure-admin to formCreate

--- src/layout/components/lay-footer/index.vue
+++ src/layout/components/lay-footer/index.vue
@@ -8,13 +8,21 @@ const TITLE = getConfig("Title");
   <footer
     class="layout-footer text-[rgba(0,0,0,0.6)] dark:text-[rgba(220,220,242,0.8)]"
   >
-    Copyright © 2020-present
+    Copyright © 2024
+    <a
+      class="hover:text-primary"
+      href="https://github.com/form-create/tutorials"
+      target="_blank"
+    >
+      &nbsp;{{ TITLE }}
+    </a>
+    &nbsp;&nbsp;|&nbsp;&nbsp;
     <a
       class="hover:text-primary"
       href="https://github.com/pure-admin"
       target="_blank"
     >
-      &nbsp;{{ TITLE }}
+      Powered by pure-admin
     </a>
   </footer>
 </template>
--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -5,5 +5,5 @@ defineOptions({
 </script>
 
 <template>
-  <h1>Pure-Admin-Thin（非国际化版本）</h1>
+  <h1>FormCreate系列教程</h1>
 </template>
